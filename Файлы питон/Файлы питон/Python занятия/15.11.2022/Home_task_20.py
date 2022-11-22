f215d94abc364af1f46035c386fb20c769281375
barboris54 <barboris229@mail.ru>
2022-11-22 18:41:28 +0300

    a123

--- Файлы питон/Файлы питон/Python занятия/15.11.2022/Home_task_20.py
+++ Файлы питон/Файлы питон/Python занятия/15.11.2022/Home_task_20.py
@@ -3,3 +3,4 @@ import re
 a = '+7 499 456-45-78, +74994564578, 7 (499) 456 45 78, 7 (499) 456-45-78'
 pattern = r'(?:\+7|7)\s*\(?499\)?\s*\d{3}.?\d{2}.?\d{2}'
 print(re.findall(pattern, a))
+print('hello')
