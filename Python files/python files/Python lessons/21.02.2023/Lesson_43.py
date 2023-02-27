cf96798700027f634b078994d59e98c195de3033
barboris54 <barboris229@mail.ru>
2023-02-28 00:10:38 +0300

    DZ_44

---
+++ Python files/python files/Python lessons/21.02.2023/Lesson_43.py
@@ -0,0 +1,22 @@
+import sqlite3 as sq
+
+with sq.connect('db_3.db') as con:
+    cur = con.cursor()
+    cur.execute('''
+    SELECT *
+    FROM T1
+    ORDER BY Fname
+    LIMIT 5 OFFSET 2
+     ''')
+
+    res = cur.fetchmany(3)
+    print(res)
+
+    # res = cur.fetchone()
+    # print(res)
+
+    # res = cur.fetchall()
+    # print(res)
+
+    # for res in cur:
+    #     print(res)
