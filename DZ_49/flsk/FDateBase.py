dbb4ecc76d12dd4fa5d4e8e135f9ca52b89eff56
barboris54 <barboris229@mail.ru>
2023-03-21 20:13:35 +0300

    DZ_49

---
+++ DZ_49/flsk/FDateBase.py
@@ -0,0 +1,22 @@
+import sqlite3
+import time
+import math
+
+
+class FDateBase:
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def get_menu(self):
+        sql = 'SELECT * FROM mainmenu'
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res:
+                return res
+        except IOError:
+            print('Ошибка чтения БД')
+        return []
+
+
---
+++ DZ_49/flsk/New_site.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+import sqlite3
+import os
+from FDateBase import FDateBase
+
+# конфигурация
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = 'sdfenfsdfhsnkdf2hy32grfwj'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(DATABASE=os.path.join(app.root_path, 'flsk.db'))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', 'r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+menu = [{'name': 'Главная', 'url': 'index'},
+        {'name': 'О нас', 'url': 'about'},
+        {'name': 'Блог', 'url': 'contact'},]
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    db = get_db()
+    dbase = FDateBase(db)
+    return render_template('index.html', title='Католог курсов', menu=dbase.get_menu())
+
+
+
+@app.route('/about')
+def about():
+    db = get_db()
+    dbase = FDateBase(db)
+    return render_template('about.html', title='О нас', menu=dbase.get_menu())
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    db = get_db()
+    dbase = FDateBase(db)
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отпралено успешно', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+        return render_template('contacts.html', title='Обратная связь', menu=dbase.get_menu())
+
+    return render_template('contacts.html', title='Обратная связь', menu=dbase.get_menu())
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu)
+
+
+@app.teardown_appcontext
+def close_db(erorr):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
